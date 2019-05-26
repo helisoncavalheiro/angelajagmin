31dfb8522643e4ea4d3fbca4a415ff989160c1f8
helison <helissoncavalheiro@gmail.com>
2019-05-26 14:19:18 -0300

    Ajustes no layout do post.
    O carrossel de imagens ainda está um pouco estranho.

---
+++ public/assets/home/js/post.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var elem = $('.carousel').carousel({
+        indicators : true,
+    });
+    var c = M.Carousel.getInstance(elem);
+
+
+});
--- resources/views/inicial/post.blade.php
+++ resources/views/inicial/post.blade.php
@@ -1,58 +1,63 @@
 @extends('layouts.home')
 
 @section('content')
-<link href="{{ asset('assets/css/home/post.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
+<link href="{{ asset('assets/home/css/post.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
+<script src="{{ asset('assets/home/js/post.js') }}" type="text/javascript" rel="script"></script>
 <main class="section">
 
-	<div class="row center-align">
-		<div class="col m2 ">
+	<div class="row">
+		<div class="col m2 show-on-medium-and-up hide-on-small-and-down">
 			<div class="card-panel">
-				<span>
-					<h6><b>Acesso Rápido</b></h6>
+				<span class="">
+					<h6 class="left-align"><b>Arquivos para Download</b></h6>
 				</span>
 				<ul class="card-content">
-					<li><a href="#">Arquivo 1</a><i class="material-icons right" >file_download</i></li>
-					<li><a href="#">Arquivo 2</a><i class="material-icons right" >file_download</i></li>
-					<li><a href="#">Arquivo 3</a><i class="material-icons right" >file_download</i></li>
-					<li><a href="#">Arquivo 4</a><i class="material-icons right" >file_download</i></li>
-					<li><a href="#">Arquivo 5</a><i class="material-icons right" >file_download</i></li>
+					<li class="valign-wrapper"><a href="#">Arquivo 1</a><i class="material-icons right" >file_download</i></li>
+					<li class="valign-wrapper"><a href="#">Arquivo 2</a><i class="material-icons right" >file_download</i></li>
+					<li class="valign-wrapper"><a href="#">Arquivo 3</a><i class="material-icons right" >file_download</i></li>
+					<li class="valign-wrapper"><a href="#">Arquivo 4</a><i class="material-icons right" >file_download</i></li>
+					<li class="valign-wrapper"><a href="#">Arquivo 5</a><i class="material-icons right" >file_download</i></li>
 				</ul>
 			</div>
-
 		</div>
 
 		<div class="col s12 m7 ">
 			<div class="card-panel">
 				<div class="row">
 					<span class="header col s12">
-						<h1 class="left-align">{{ $post->title }}</h1>
+						<h1 class="left-align post-title">{{ $post->title }}</h1>
 					</span>
 				</div>
 				<div class="row">
+					<!-- Última atualização do post -->
+					<p class="col s3 post-update valign-wrapper"><i class="material-icons left tiny">access_time</i> {{ $post->updated_at  }}</p>
+					<!--Autor do post -->
+					<p class="col s5pp post-author valign-wrapper"><i class="material-icons left tiny">account_circle</i> Ângela Jagmin Carretta </p>
+				</div>
+				<div class="row">
 					<div class="col s12">
-						<span class="flow-text publicacao-texto">
-						<?php echo $post->content;  ?>
+						<span class="publicacao-texto">
+							<?php echo $post->content;  ?>
 						</span>
 					</div>
 				</div>
-			</div>
-		</div>
 
-		<div class="col m3 hide-on-med-and-down">
-			<div class="card-panel">
-				<span class="card-title">
-					<h4>Mais Clicadas:</h4>
-				</span>
-				<div class="card-content row">
-					<ul class="col s10 push-m2">
-						<li><a href="#">Jogos para ensino da matemática</a></li>
-						<br>
-						<li><a href="#">Trabalhos publicados no CONGREGA</a></li>
-						<br>
-						<li><a href="#">Test 1 </a></li>
-						<br>
-						<li><a href="#">Test 1 </a></li>
-					</ul>
+				<div class="row valign-wrapper">
+
+						<i class="deep-orange lighten-1 material-icons medium left">chevron_left</i>
+
+					<div class="carousel carousel-slider">
+						@foreach($post->images as $i)
+						<div class="col s10">
+							<a class="carousel-item" href="#one!">
+								<img src="{{ asset($i->filepath) }}">
+							</a>
+						</div>
+						@endforeach
+					</div>
+					<div class="col s1 deep-orange lighten-1 circle slider-button valign-wrapper">
+						<i class="left material-icons medium">chevron_right</i>
+					</div>
 				</div>
 			</div>
 		</div>
